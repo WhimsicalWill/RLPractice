4ef8457da5f9abbb2f06c3b1e598590a4fc90893
whimsicalwill <knipewilliam20@gmail.com>
2022-07-09 17:27:36 -0400

    Load environment data

--- mujocotest.py
+++ mujocotest.py
@@ -1,19 +1,46 @@
-import gym
 import mujoco_py
-from gym.wrappers import Monitor
+import gym
+import pickle
 
-env = gym.make('Reacher-v2')
-env = Monitor(env, 'recording', video_callable=lambda episode_id: episode_id==1, force=True)
+def save_best(env):
+	print("new best")
+	print(env.sim.get_state())
+	with open('state_data.pkl', 'wb') as file:
+		pickle.dump(env.sim.get_state(), file, pickle.HIGHEST_PROTOCOL)
 
-max_episodes = 100
+def load_best():
+	print("loading best")
+	with open('state_data.pkl', 'rb') as file:
+		state_data = pickle.load(file)
+		print(state_data)
+
+env = gym.make('HalfCheetah-v3')
+
+max_episodes = 10
 max_time_steps = 300
 
+best_score = float('-inf')
 for i in range(max_episodes):
 	env.reset()
+	# test saving env info
+	# print(env.sim.get_state())
+	score = 0
 	for t in range(max_time_steps):
-		env.render()
+		render_img = env.render(
+			mode="rgb_array",
+			width=64,
+			height=64,
+		)
+		# print(rgb_array_large.shape)
 		new_state, reward, done, _ = env.step(env.action_space.sample())
-		
+		score += reward
 		if done:
 			break
-env.close()
+	print(f"Episode {i}: score {score}")
+	if score > best_score:
+		best_score = score
+		save_best(env)
+
+load_best()
+
+
